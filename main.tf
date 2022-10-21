9c798b2bb46907996f2c87f1965c87b4cab0712e
Joseph Ligier <joseph.ligier@accenture.com>
2022-10-21 12:50:24 +0200

    FIX Improve of images or not

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 locals {
-  main_image      = contains(values(var.images), "main") ? regex("^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", var.images.main) : {}
-  operator_image  = contains(values(var.images), "operator") ? regex("^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", var.images.operator) : {}
-  preflight_image = contains(values(var.images), "preflight") ? regex("^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", var.images.preflight) : {}
+  main_image      = contains(keys(var.images), "main") ? regex("^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", var.images.main) : {}
+  operator_image  = contains(keys(var.images), "operator") ? regex("^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", var.images.operator) : {}
+  preflight_image = contains(keys(var.images), "preflight") ? regex("^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", var.images.preflight) : {}
 
   main_set_values      = local.main_image != {} ? [{ name = "image.repository", value = "${local.main_image.url}/${local.main_image.image}" }, { name = "image.useDigest", value = "false" }] : []
-  operator_set_values  = local.operator_image != {} ? [{ name = "operator.image.repository", value = "${local.main_image.url}/${local.main_image.image}" }, { name = "operator.image.useDigest", value = "false" }] : []
-  preflight_set_values = local.preflight_image != {} ? [{ name = "preflight.image.repository", value = "${local.main_image.url}/${local.main_image.image}" }, { name = "preflight.image.useDigest", value = "false" }] : []
+  operator_set_values  = local.operator_image != {} ? [{ name = "operator.image.repository", value = "${local.operator_image.url}/${local.operator_image.image}" }, { name = "operator.image.useDigest", value = "false" }] : []
+  preflight_set_values = local.preflight_image != {} ? [{ name = "preflight.image.repository", value = "${local.preflight_image.url}/${local.preflight_image.image}" }, { name = "preflight.image.useDigest", value = "false" }] : []
 
   set_values = concat(var.set_values, local.main_set_values, local.operator_set_values, local.preflight_set_values)
 }
