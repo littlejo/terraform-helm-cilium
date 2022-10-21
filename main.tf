4f5b13a0a61a133efa6d7432e3f412b9aad6a914
Joseph Ligier <joseph.ligier@accenture.com>
2022-10-21 13:42:57 +0200

    Improve preflight

--- main.tf
+++ main.tf
@@ -3,9 +3,24 @@ locals {
   operator_image  = contains(keys(var.images), "operator") ? regex("^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", var.images.operator) : {}
   preflight_image = contains(keys(var.images), "preflight") ? regex("^(?:(?P<url>[^/]+))?(?:/(?P<image>[^:]*))??(?::(?P<tag>[^:]*))", var.images.preflight) : {}
 
+  default_preflight_set_values = [
+    {
+      name  = "preflight.enabled"
+      value = "true"
+    },
+    {
+      name  = "agent"
+      value = "false"
+    },
+    {
+      name  = "operator.enabled"
+      value = "false"
+    },
+  ]
+
   main_set_values      = local.main_image != {} ? [{ name = "image.repository", value = "${local.main_image.url}/${local.main_image.image}" }, { name = "image.useDigest", value = "false" }] : []
   operator_set_values  = local.operator_image != {} ? [{ name = "operator.image.repository", value = "${local.operator_image.url}/${local.operator_image.image}" }, { name = "operator.image.useDigest", value = "false" }] : []
-  preflight_set_values = local.preflight_image != {} ? [{ name = "preflight.image.repository", value = "${local.preflight_image.url}/${local.preflight_image.image}" }, { name = "preflight.image.useDigest", value = "false" }] : []
+  preflight_set_values = local.preflight_image != {} ? concat([{ name = "preflight.image.repository", value = "${local.preflight_image.url}/${local.preflight_image.image}" }, { name = "preflight.image.useDigest", value = "false" }], local.default_preflight_set_values) : []
 
   set_values = concat(var.set_values, local.main_set_values, local.operator_set_values, local.preflight_set_values)
 }
