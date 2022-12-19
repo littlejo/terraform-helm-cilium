07b9830bc468d991c1490dab9ad328a63e575e09
Joseph Ligier <joseph.ligier@accenture.com>
2022-12-19 12:00:03 +0100

    Fix

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "main_image" {
 module "operator_image" {
   source     = "github.com/littlejo/terraform-helm-images-set-values"
   repo_regex = var.repo_regex
-  repo_url   = var.images.main
+  repo_url   = var.images.operator
   pre_value  = "operator.image"
   type       = "cilium"
 }
@@ -49,7 +49,7 @@ module "operator_image" {
 module "preflight_image" {
   source     = "github.com/littlejo/terraform-helm-images-set-values"
   repo_regex = var.repo_regex
-  repo_url   = var.images.main
+  repo_url   = var.images.preflight
   pre_value  = "preflight.image"
   type       = "cilium"
 }
